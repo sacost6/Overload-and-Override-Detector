e23f9697c41cc1eb3f8684ca100d8c3ab05a419a
Simon <sacost6@uic.edu>
2021-02-04 19:04:14 -0600

    First commit

---
+++ src/main/java/edu/uic/cs474/a2/Main.java
@@ -0,0 +1,13 @@
+package edu.uic.cs474.a2;
+
+import edu.uic.cs474.a2.solution.A2Solution;
+
+public class Main {
+
+    private Main() { /* Empty */ }
+
+    static ReflectionUtils getReflectionUtils() {
+        return new A2Solution();
+    }
+
+}
